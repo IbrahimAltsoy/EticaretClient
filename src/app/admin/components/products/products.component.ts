ba0e1bd39073715bfef57453c656f59ff60e76cf
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2022-12-12 18:46:27 +0300

    19. Services added. Crud transactions started. The data console has been pressed. Create was created from crud operations.

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -8,13 +9,49 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
-  constructor( spinner: NgxSpinnerService){
+  constructor( spinner: NgxSpinnerService, private httpClientService: HttpClientService){
     super(spinner)
   }
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallSpin)
 
-
-
+  }
 }
-}
+
+    // this.showSpinner(SpinnerType.BallSpin);
+    // this.httpClientService.get<Create_Product[]>({
+    //   controller :"products"
+    // }).subscribe(data=>console.log(data));
+
+    // this.httpClientService.post({
+    //   controller: "products"
+    // },{
+    //   name: "Kombi",
+    //   stock: 150,
+    //   price: 270
+    // }).subscribe();
+    // this.httpClientService.post({
+    //   controller: "products"
+    // },{
+    //   name: "Terlik",
+    //   stock: 250,
+    //   price: 370
+    // }).subscribe();
+    // this.httpClientService.put({
+    //   controller: "products",
+    // },{
+    //   id: "c836ca4b-c025-4eb4-bb41-111ad7a06375",
+    //   name: "Sandalet",
+    //   stock: 101,
+    //   price: 3
+    // }).subscribe();
+    // this.httpClientService.delete({
+    //   controller: "products"
+    // },
+    // "610b5ad9-fde7-4957-b7d8-b5feefb7ebd5"
+    // ).subscribe();
+    // this.httpClientService.get({
+    //  fullEndPoint: "https://jsonplaceholder.typicode.com/posts"
+    // }).subscribe(data=> console.log(data));
+
+
+
---
+++ src/app/services/common/http-client.service.ts
@@ -0,0 +1,72 @@
+import { Inject, Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(private httpClient: HttpClient, @Inject("baseUrl") private baseUrl: string ) {}
+
+  private url(requestParameters: Partial<RequestParameters>): string{
+    return `${requestParameters.baseUrl ? requestParameters.baseUrl : this.baseUrl}/${requestParameters.controller}${requestParameters.action ?`/${requestParameters.action}` : ""}`;
+
+
+  }
+
+
+
+  get<T>(requestParameters: Partial<RequestParameters>, id?:string): Observable<T>{
+    let url:string = "";
+    if(requestParameters.fullEndPoint)
+    url = requestParameters.fullEndPoint;
+    else
+    url = `${this.url(requestParameters)} ${id ? `/${id}`: ""}`;
+    return this.httpClient.get<T>(url, {headers: requestParameters.header})
+
+
+
+  }
+  post<T>(requestParameters: Partial<RequestParameters>, body: Partial<T>): Observable<T>{
+    let url: string = "";
+    if(requestParameters.fullEndPoint)
+    url = requestParameters.fullEndPoint;
+    else
+    url = `${this.url(requestParameters)}`
+    return this.httpClient.post<T>(url, body, {headers: requestParameters.header});
+
+  }
+  put<T>(requestParameters: Partial<RequestParameters>, body: Partial<T>): Observable<T>{
+    let url: string ="";
+    if(requestParameters.fullEndPoint)
+    url = requestParameters.fullEndPoint;
+    else
+    url = `${this.url(requestParameters)}`;
+    return this.httpClient.put<T>(url, body, {headers:requestParameters.header});
+
+  }
+  // Delete dikkat et hatalı olabilir silme işlemi başarılı olmasına rağmen hata oluşturuyor
+  delete<T>(requestParameters: Partial<RequestParameters>, id: string): Observable<T>{
+    let url:string = "";
+    if(requestParameters.fullEndPoint)
+    url = requestParameters.fullEndPoint;
+    else
+    url = `${this.url(requestParameters)}/${id}`;
+    return this.httpClient.delete<T>(url, {headers:requestParameters.header});
+
+  }
+
+}
+
+export class RequestParameters{
+  controller?: string;
+  action?: string;
+  header?: HttpHeaders;
+  baseUrl?: string;
+  fullEndPoint?:string;
+
+
+
+
+}
